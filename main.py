cefe8e10db3bf478ef14ebd99b3c9f298edd6a95
sardoruktamov <73293096+sardoruktamov@users.noreply.github.com>
2021-09-26 18:24:10 +0500

    full complated

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 from transliterate import to_latin, to_cyrillic
+
+
+text = input("matn kiriting: ")
+
+if text.isascii():                  #agar kiritilgan matn lotin alifbosida (isascii jadvalida) bolsa
+    print(to_cyrillic(text))
+else:
+    print(to_latin(text))
